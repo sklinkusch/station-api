1df098a646aaffdc1e6e7bfed9c54eb05b44187e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-01-22 15:21:32 +0100

    reduce things to one endpoint with multiple parameters

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ const app = express();
 const port = 3000;
 const stations = require('vbb-stations/full.json');
 
-app.get('/station', (req, res) => {
+app.get('/', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
-  const { inputName } = req.query;
-  const filteredStations = Object.values(stations).filter(station => station.name.toLowerCase().includes(inputName.toLowerCase()));
-  const modifiedStations = filteredStations.map(station => {
-    const { name, id, ...remainingStation } = station;
-    const idParts = (id.split(/:+/))
-    const newId = idParts[2];
-    if (name.endsWith("(Berlin)")) {
-      const modifiedNameA = name.replace(" (Berlin)", "");
-      const modifiedNameB = "Berlin, " + modifiedNameA;
-      return { ...remainingStation, id: newId, name: modifiedNameB };
+  const { station: inputName, id: inputId } = req.query;
+  if (inputName) {
+    const filteredStations = Object.values(stations).filter(station => station.name.toLowerCase().includes(inputName.toLowerCase()));
+    const modifiedStations = filteredStations.map(station => {
+      const { name: oldName, id: oldId, ...remainingStation } = station;
+      const idParts = (oldId.split(/:+/))
+      const newId = idParts[2];
+      if (oldName.endsWith("(Berlin)")) {
+        const modifiedNameA = oldName.replace(" (Berlin)", "");
+        const modifiedNameB = "Berlin, " + modifiedNameA;
+        return { ...remainingStation, id: newId, name: modifiedNameB };
+      }
+      return { ...remainingStation, name: oldName, id: newId };
+    })
+    const stationIds = modifiedStations.map(station => station.id);
+    const uniqueStationIdSet = new Set(stationIds);
+    const uniqueStationIds = Array.from(uniqueStationIdSet);
+    const uniqueStations = uniqueStationIds.map(id => modifiedStations.filter(station => station.id === id)[0]);
+    console.log(uniqueStations);
+    const sortedStations = uniqueStations.sort((a, b) => {
+      console.log(a.name, b.name);
+      return a.name.localeCompare(b.name, 'de', { sensitivity: 'base' })
+    });
+    if (sortedStations.length > 0) {
+      return res.status(200).json(sortedStations);
+    } else {
+      return res.status(204).json(sortedStations);
     }
-    return { ...remainingStation, name, id: newId };
-  })
-  const stationIds = modifiedStations.map(station => station.id);
-  const uniqueStationIdSet = new Set(stationIds);
-  const uniqueStationIds = Array.from(uniqueStationIdSet);
-  const uniqueStations = uniqueStationIds.map(id => modifiedStations.filter(station => station.id === id)[0]);
-  const sortedStations = uniqueStations.sort((a, b) => a.name.localeCompare(b.name, 'de', { sensitivity: 'base' }));
-  if (sortedStations.length > 0) {
-    return res.status(200).json(sortedStations);
-  } else {
-    return res.status(204).json(sortedStations);
-  }
-});
-
-app.get('/id', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  const { id } = req.query;
-  const mappedStations = Object.values(stations).map(station => {
+  } else if (inputId) {
+    const mappedStations = Object.values(stations).map(station => {
     const { id: oldStationId, name: oldStationName } = station
     const idStationArray = oldStationId.split(/:+/)
     const newId = idStationArray[2]
@@ -44,18 +44,17 @@ app.get('/id', (req, res) => {
       const modifiedNameB = `Berlin, ${modifiedNameA}`
       return { ...station, id: newId, name: modifiedNameB }
     }
-    return { ...station, id: newId }
+    return { ...station, id: newId, name: oldStationName }
   });
-  const filteredStations = mappedStations.filter(station => station.id === id);
+  const filteredStations = mappedStations.filter(station => station.id === inputId);
   if (filteredStations.length >= 1) {
     const station = filteredStations[0];
     return res.status(200).json(station);
   } 
   return res.status(204).json({});
-});
-
-app.all('*', (req, res) => {
-  return res.status(404).json({ error: { message: 'The endpoint does not exist.' }});
+  } else {
+    return res.status(422).json({error: { message: "Insufficient parameters. Please provide either id or station."}});
+  }
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
